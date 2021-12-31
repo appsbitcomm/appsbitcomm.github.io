1d8c2ed81b4c7e1a136c8bbee5d3ab308dc6a922
Jay Saha <jay.saha@bitcommtechnologies.com>
2021-12-31 18:31:54 +0530

    Changed RDPMS Model

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "3eee092883b6845382c372c0b4345a79",
-"index.html": "a8765b17a7cbd3ef6b90247d8a3f65a2",
-"/": "a8765b17a7cbd3ef6b90247d8a3f65a2",
-"main.dart.js": "d5e1a34e25b05b17cf92c7272631e4f2",
+  "version.json": "d009ae1bcba7a632cb821ecaccd2c75d",
+"index.html": "cd8dff3aedca3abaccb485f2929631c1",
+"/": "cd8dff3aedca3abaccb485f2929631c1",
+"main.dart.js": "497c0b690708b9f901d9ec0a21722eb7",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -14,10 +14,14 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "feed8862ba7e6c1ab7c7ad67e9880cc3",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "93076f02a9a62f01d6760ce7cc11766f",
+"assets/NOTICES": "29b01626aea9dbbba81ff1fb86c4ca24",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1"
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"canvaskit/canvaskit.js": "43fa9e17039a625450b6aba93baf521e",
+"canvaskit/profiling/canvaskit.js": "f3bfccc993a1e0bfdd3440af60d99df4",
+"canvaskit/profiling/canvaskit.wasm": "a9610cf39260f60fbe7524a785c66101",
+"canvaskit/canvaskit.wasm": "04ed3c745ff1dee16504be01f9623498"
 };
 
 // The application shell files that are downloaded before a service worker can
